be3f49642505df7f50ac0c1732544b21be23ca22
mBentaifour <bentaifourmoh@gmail.com>
2024-03-31 11:17:21 -0700

    lifo and Fifo

--- errors.c
+++ errors.c
@@ -90,11 +90,12 @@ void more_err(int error_code, ...)
 }
 
 /**
- * string_err - handles errors.
+ * string_err - handles errors
  * @error_code: The error codes are the following:
- * (10) ~> The number inside a node is outside ASCII bounds.
- * (11) ~> The stack is empty.
+ * (10) ~> the number inside a node is outside ASCII bounds
+ * (11) ~> the stack is empty
  */
+
 void string_err(int error_code, ...)
 {
 	va_list ag;
